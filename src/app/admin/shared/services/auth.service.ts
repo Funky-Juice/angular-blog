f49e3bfc224e8dbec4fca030f7f4d8125eb294ba
Funky-Juice <isa86box@gmail.com>
2020-01-08 01:36:02 +0300

    Firebase setup

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../../../shared/interfaces';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+import {FbAuthResponse, User} from '../../../shared/interfaces';
+import {environment} from '../../../../environments/environment';
 
 @Injectable()
 
@@ -14,7 +17,10 @@ export class AuthService {
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user);
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      );
   }
 
   logout() {
@@ -24,6 +30,7 @@ export class AuthService {
     return !!this.token;
   }
 
-  private setToken() {
+  private setToken(res: FbAuthResponse) {
+    console.log(res);
   }
 }
