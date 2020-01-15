fa1474847fd5b66677bab702ae0f51871df02467
Иншаков Сергей <inshakovsa@edu.mos.ru>
2020-01-15 12:22:39 +0300

    posts list display

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from '../../shared/posts.service';
+import {Subscription} from 'rxjs';
+import {Post} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  pSub: Subscription;
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
+    this.pSub = this.postsService.getAll().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  remove(id: string) {
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
   }
 }
